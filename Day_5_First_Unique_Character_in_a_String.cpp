d6b1e842ef2d08993b24e5f82680eae7e96c1f5c
Khimraj <khimrajsuthar@gmail.com>
2020-05-16 15:30:31 +0530

    Day 5

--- Day_5_First_Unique_Character_in_a_String.cpp
+++ Day_5_First_Unique_Character_in_a_String.cpp
@@ -23,7 +23,7 @@ public:
             count[c]++;
         }
         for (int i = 0; i < s.size(); i++) {
-            if (count[s[i]] == 1){
+            if (count[s[i]] == 1) {
                 return i;
             }
         }
